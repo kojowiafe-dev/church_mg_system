8c518d0adbcfa44712f83462d8f91af76761f21f
kojowiafe-dev <kojowiafe502@gmail.com>
2025-08-24 02:39:55 +0000

    Removed backend folder

--- backend/app/utils/otp.py
+++
@@ -1,9 +0,0 @@
-import random
-from datetime import datetime, timedelta
-
-def generate_otp():
-    return str(random.randint(100000, 999999))
-
-
-def get_expiry(minutes=10):
-    return datetime.utcnow() + timedelta(minutes=minutes)
